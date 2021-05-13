842652e34a1258c6917ad1c60b2cd41d3727dc91
Jess-Sousa <jess.sousak@gmail.com>
2021-05-13 16:07:28 -0300

    Create aulapratica1.java

---
+++ aulapratica1.java
@@ -0,0 +1,33 @@
+
+/**
+ * # Sistema_de_informacao_de_banco_de_dados
+Objetivo: Armazenar as atividades propostas e futuras anotações sobre a criação do projeto avaliativo.
+ * @author (Jéssica Sousa) 
+ * @version (v1 13/05)
+ */
+public class AtividadePratica1
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int x;
+
+    /**
+     * Construtor para objetos da classe AtividadePratica1
+     */
+    public AtividadePratica1()
+    {
+        // inicializa variáveis de instância
+        x = 0;
+    }
+
+    /**
+     * Um exemplo de um método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   um exemplo de um parâmetro de método
+     * @return     a soma de x e y 
+     */
+    public int sampleMethod(int y)
+    {
+        // escreva seu código aqui
+        return x + y;
+    }
+}
