483c32f45d477841b31c66b292209c5da9ff6bbe
Jéssica Sousa <jess.sousak@gmail.com>
2021-05-25 11:41:32 -0300

    Commit

---
+++ Fabrica/Motocicleta.java
@@ -0,0 +1,30 @@
+package Fabrica;
+
+
+
+public class Motocicleta
+{
+    //Aqui declaramos as variáveis
+    private String marca = Bross;
+    private int anoFab = 1960;
+    private float cachi = 234960;
+    private int velocimetro;
+    private int freio;
+}
+public Motocicleta
+{
+       marca = Bross;
+       anoFab = 1960;
+       Cachi = 234960;
+} 
+
+public void velocimentro (int velocimetro) {
+    velocimentro += velocimetro;
+}
+    public int getvelocimetro(){
+        return  ;
+    }
+    /* aqui pode ser colocado os comentários
+     * 
+     */
+}
